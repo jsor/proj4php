25fb79613fbdeff88ca2ca724903b9e72aab5209
julien2512 <julien2512@7b26fc74-cbda-4134-b5c1-02737463f037>
2012-05-15 18:13:15 +0000

    testproj.php relative path.
    git-svn-id: https://svn.code.sf.net/p/proj4php/code/trunk@10 7b26fc74-cbda-4134-b5c1-02737463f037

--- test/testproj.php
+++ test/testproj.php
@@ -1,11 +1,12 @@
 <?php
-include_once("../proj4php/proj4php.php");
+include_once("../src/proj4php/proj4php.php");
 
 $proj4 = new Proj4php();
 $projL93 = new Proj4phpProj('EPSG:2154',$proj4);
 $projWGS84 = new Proj4phpProj('EPSG:4326',$proj4);
 $projLI = new Proj4phpProj('EPSG:27571',$proj4);
 $projLSud = new Proj4phpProj('EPSG:27563',$proj4);
+$projL72 = new Proj4phpProj('EPSG:31370',$proj4);
 
 // GPS
 // latitude        longitude
@@ -21,6 +22,16 @@ $projLSud = new Proj4phpProj('EPSG:27563',$proj4);
 $pointSrc = new proj4phpPoint('652709.401','6859290.946');
 echo "Source : ".$pointSrc->toShortString()." in L93 <br>";
 $pointDest = $proj4->transform($projL93,$projWGS84,$pointSrc);
+echo "Conversion : ".$pointDest->toShortString()." in WGS84<br><br>";
+
+$pointSrc = $pointDest;
+echo "Source : ".$pointSrc->toShortString()." in WGS84<br>";
+$pointDest = $proj4->transform($projWGS84,$projL72,$pointSrc);
+echo "Conversion : ".$pointDest->toShortString()." in Lambert 1972<br><br>";
+
+$pointSrc = $pointDest;
+echo "Source : ".$pointSrc->toShortString()." in Lambert 1972<br>";
+$pointDest = $proj4->transform($projL72,$projWGS84,$pointSrc);
 echo "Conversion : ".$pointDest->toShortString()." in WGS84<br><br>";
 
 $pointSrc = $pointDest;
