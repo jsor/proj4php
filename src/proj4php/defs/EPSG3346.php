15ccf71d175163e0e125b9ebfe30792841780fb2
JanisVepris <janis.vepris@nfq.lt>
2015-06-05 16:31:48 +0300

    Added EPSG:3346 (a.k.a LKS-94) definition, found at http://epsg.io/3346

---
+++ src/proj4php/defs/EPSG3346.php
@@ -0,0 +1,2 @@
+<?php
+Proj4php::$defs['EPSG:3346'] = "+title=LKS94 / Lithuania TM +proj=tmerc +lat_0=0 +lon_0=24 +k=0.9998 +x_0=500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs";
